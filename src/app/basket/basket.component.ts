fd22f51d2387262f3183cc1e0af303c83de58c6d
yhlukhov <yhlukhov@gmail.com>
2020-08-16 17:43:39 +0300

    completed basket page

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { OrderService } from 'src/shared/services/order.service';
 import { IProduct } from 'src/shared/interfaces/product.interface';
+
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
@@ -8,13 +10,53 @@ import { IProduct } from 'src/shared/interfaces/product.interface';
 })
 export class BasketComponent implements OnInit {
   order:Array<IProduct> = []
-  constructor(private orderService:OrderService) { }
+  totalPrice = 0
+  modalRef: BsModalRef
+  remProd:IProduct
+
+  constructor(private orderService:OrderService, private modalService: BsModalService) { }
+
   ngOnInit(): void {
     this.loadOrder()
+    this.getLocalStorage()
   }
+
   loadOrder() {
     if(localStorage.length>0 && localStorage.getItem('myOrder'))
       this.order = JSON.parse(localStorage.getItem('myOrder'))
-    console.log(this.order)
+  }
+
+  private getLocalStorage() {
+    if (localStorage.length > 0 && localStorage.getItem('myOrder')) {
+      const localProducts = JSON.parse(localStorage.getItem('myOrder'))
+      this.totalPrice = localProducts.reduce((total, prod) => {
+        return total + prod.count*prod.price
+      }, 0)
+    }
+  }
+
+  increase(product, sign) {
+    let prod = this.order.find(p => p.id === product.id)
+    if (sign === "+")
+    prod.count++
+    else if(product.count>1)
+    prod.count--
+    localStorage.setItem('myOrder', JSON.stringify(this.order))
+    this.getLocalStorage()
+    this.orderService.basket.next(product)
+  }
+
+  remove() {
+    let index = this.order.findIndex(p => p.id === this.remProd.id)
+    this.order.splice(index, 1)
+    localStorage.setItem('myOrder', JSON.stringify(this.order))
+    this.getLocalStorage()
+    this.orderService.basket.next(this.remProd)
+    this.modalRef.hide()
+  }
+
+  openModal(template: TemplateRef<any>, remProd:IProduct) {
+    this.modalRef = this.modalService.show(template)
+    this.remProd = remProd
   }
 }
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/shared/services/product.service';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/shared/interfaces/product.interface';
+import { OrderService } from '../../shared/services/order.service';
 
 @Component({
   selector: 'app-product-details',
@@ -14,6 +15,7 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private prodService: ProductService,
+    private orderService: OrderService,
     private actRoute: ActivatedRoute,
   ) { }
 
@@ -26,5 +28,29 @@ export class ProductDetailsComponent implements OnInit {
     this.prodService.getOneProduct(id).subscribe(data =>{
       this.product = data
     })
+  }
+
+  increase(sign) {
+    if(sign === '+') this.product.count++
+    else if (this.product.count > 1) this.product.count--
+  }
+
+  makeOrder() {
+    let localProducts:Array<IProduct> = []
+    if (localStorage.getItem('myOrder')) {
+      localProducts = JSON.parse(localStorage.getItem('myOrder'))
+      let localProduct = localProducts.find(prod => prod.id === this.product.id)
+      if(localProduct) {
+        localProduct.count += this.product.count
+      }
+      else {
+        localProducts.push(this.product)
+      }
+    }
+    else {
+      localProducts.push(this.product)
+    }
+    localStorage.setItem('myOrder', JSON.stringify(localProducts))
+    this.orderService.basket.next(this.product)
   }
 }
